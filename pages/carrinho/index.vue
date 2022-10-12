82b6ecb00fb15140b6c413e58ea8fe8c63c94f0a
Flávio Gomes <the.skuth@gmail.com>
2022-10-12 01:27:45 -0300

    Feat: Creating cart page to list products from storage

---
+++ pages/carrinho/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <pre>{{ cartItems }}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import { useCart } from "@/store/cart";
+
+export default {
+  setup() {
+    const cart = useCart();
+    const cartItems = computed(() => cart.getCart);
+
+    return {
+      cartItems,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
